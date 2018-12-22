eb15e64b4822f94a0b52906838b4eab22a3af71d
Miguel Angel Fuentes <mikeegivieer@gmail.com>
2018-12-22 14:19:48 -0600

    Funciones

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Sacha', edad=28
+
+function imprimirEdad(n, e){
+
+    console.log( ` ${n} tiene ${e} años `)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('vicky', 28)
+imprimirEdad('dario',24)
