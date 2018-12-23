85a7d4837faa4f91943d67dea1430915e3a98468
Miguel Angel Fuentes <mikeegivieer@gmail.com>
2018-12-22 18:00:18 -0600

    Introducción a arrays

---
+++ clase17.js
@@ -0,0 +1,15 @@
+var victoria = {
+    nombre: 'victoria',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+
+var paula = {
+    nombre: 'paula',
+    apellido: 'Fuentes',
+    altura: 1.34
+}
+
+var personas = [victoria, paula]
+
+
