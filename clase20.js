fc15e265f1d278537fc1eaf5ea95e67205d91db2
Miguel Angel Fuentes <mikeegivieer@gmail.com>
2018-12-22 19:03:47 -0600

    Reducir un array a un valor

---
+++ clase20.js
@@ -0,0 +1,65 @@
+var victoria = {
+    nombre: 'victoria',
+    apellido: 'Zapata',
+    altura: 1.56,
+    cantidadDeLibros: 29
+}
+
+var paula = {
+    nombre: 'paula',
+    apellido: 'Fuentes',
+    altura: 1.34,
+    cantidadDeLibros: 34
+
+}
+
+var marcelo = {
+    nombre: 'marcelo',
+    apellido: 'Fuentes',
+    altura: 1.34,
+    cantidadDeLibros: 288
+
+}
+
+const esAlta =({altura})=> altura > 1.4
+
+var personas = [victoria, paula, marcelo]
+
+//filter va iterar por nosotros el array
+var personasAltas = personas.filter(esAlta)
+
+// var personasAltas = personas.filter(function (persona){
+//     return persona.altura > 1.4
+// })
+
+
+// const pasarAlturaACms =persona=>{
+// //   persona.altura*=100
+// //Regresamos un nuevo objeto 
+//   return {
+//       ...persona,
+//       altura: persona.altura *100
+//   }
+// }
+//hace lo mismo que arriba 
+const pasarAlturaACms =persona=>({
+          ...persona,
+          altura: persona.altura *100
+      })
+    
+
+
+//map nos regresa un nuevo array construido apartir de una condicion
+var personasCms = personas.map(pasarAlturaACms)
+
+const reducer = (acum, {cantidadDeLibros})=> acum+ cantidadDeLibros
+
+
+//reduce un array a un valor unico 
+var totalDeLibros = personas.reduce(reducer,0) 
+
+
+
+
+console.log(personasCms)
+
